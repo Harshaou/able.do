2cc497b1707d003d64c6a9edfea33cbcf44574f7
Harshad <harsh.haou@gmail.com>
2022-05-12 09:51:43 +0530

    Developing UI

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ function App() {
     }
   };
 
+  function getRandomColor() {
+    var letters = 'BCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+  }
+
   return (
     <div className="container">
       <h1 className="title">Generate Combinations</h1>
@@ -32,19 +41,28 @@ function App() {
           <button onClick={handleChange}>Generate Combinations</button>
         </div>
         <div className="display-section">
-          <h4>Result</h4>
+          <h4>Result: {output?.length}</h4>
           {output?.length > 0 &&
-            output.map((arr, index) =>
-              arr.map((item) => {
-                return (
-                  <div key={index}>
-                    <p style={{ display: 'inline' }}>
-                      {item.color}, {item.length}, {item.finish}
-                    </p>
-                  </div>
-                );
-              })
-            )}
+            output.map((arr, index) => {
+              let result = JSON.stringify(arr)
+                .slice(1, -1)
+                .replace(/[{}]/g, '')
+                .replaceAll(',', '|')
+                .replaceAll('"', ' ');
+              return (
+                <div>
+                  <p
+                    className="item"
+                    key={index}
+                    style={{
+                      backgroundColor: getRandomColor(),
+                    }}
+                  >
+                    {result}
+                  </p>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
